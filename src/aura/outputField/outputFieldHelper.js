b2cea24ea3409eac559e7ce84600a912e2db014b
Simranjeet Singh <simran.sf.dev@gmail.com>
2017-07-13 13:59:51 +1000

    Update outputFieldHelper.js

--- src/aura/outputField/outputFieldHelper.js
+++ src/aura/outputField/outputFieldHelper.js
@@ -55,11 +55,96 @@
         } 
         else if (fieldType == "URL"){
             uiComponentType = "ui:outputURL";
-        } 
+        }
+        else if (fieldType == "REFERENCE"){
+            uiComponentType = "c:referenceField";
+        }
         else {
             uiComponentType = "ui:outputText";
         }
         
         return uiComponentType;
-	}
+	},
+    
+    createField : function(component, event, uiComponentType){
+        $A.createComponent(
+            uiComponentType,
+            {
+                "value": component.get("v.value"),
+                "class": component.get("v.class")
+            },
+            function(uiComponent, status, errorMessage){
+                if (status === "SUCCESS") {
+                    var componentBody = component.get("v.body");
+                    componentBody.push(uiComponent);
+                    component.set("v.body", componentBody);
+                }
+                else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.")
+                }
+                else if (status === "ERROR") {
+                    console.log("Error: " + errorMessage);
+                }
+            }
+        );
+    },
+
+    createURLField : function(component, event){
+        var urlLabel = component.get("v.label");
+        if (urlLabel == null || urlLabel == undefined || urlLabel == ""){
+            urlLabel = component.get("v.value");
+        }
+        
+        $A.createComponent(
+            "ui:outputURL",
+            {
+                "value": component.get("v.value"),
+                "label": urlLabel,
+                "class": component.get("v.class")
+            },
+            function(urlField, status, errorMessage){
+                if (status === "SUCCESS") {
+                    var componentBody = component.get("v.body");
+                    componentBody.push(urlField);
+                    component.set("v.body", componentBody);
+                }
+                else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.")
+                }
+                else if (status === "ERROR") {
+                    console.log("Error: " + errorMessage);
+                }
+            }
+        );
+    },
+
+    createReferenceField : function(component, event){
+        var fieldLabel = component.get("v.label");
+        if (fieldLabel == null || fieldLabel == undefined || fieldLabel == ""){
+            fieldLabel = component.get("v.value");
+        }
+        
+        $A.createComponent(
+            "c:referenceField",
+            {
+                "referenceRecordId": component.get("v.value"),
+                "referenceRecordName": fieldLabel,
+                "class": component.get("v.class")
+            },
+            function(refField, status, errorMessage){
+                if (status === "SUCCESS") {
+                    var componentBody = component.get("v.body");
+                    componentBody.push(refField);
+                    component.set("v.body", componentBody);
+                }
+                else if (status === "INCOMPLETE") {
+                    console.log("No response from server or client is offline.")
+                }
+                else if (status === "ERROR") {
+                    console.log("Error: " + errorMessage);
+                }
+            }
+        );
+    }
+
 })
